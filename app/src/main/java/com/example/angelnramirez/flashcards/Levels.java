d022f551337a8ba8bf3f5ece5c3f647df40410bf
Aspharr17 <45318205+Aspharr17@users.noreply.github.com>
2020-01-20 23:41:33 -0600

    DbHelper cleaned

---
+++ app/src/main/java/com/example/angelnramirez/flashcards/Levels.java
@@ -0,0 +1,13 @@
+package com.example.angelnramirez.flashcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Levels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_levels);
+    }
+}
